c5be726ab76d928cc2fd2bf76b611cac610cadbb
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-03-24 17:28:27 +0000

    Add missing blank line

--- pytorch/models_pytorch.py
+++ pytorch/models_pytorch.py
@@ -30,4 +30,9 @@ def move_data_to_gpu(x, cuda):
 def init_layer(layer):
     """Initialize a Linear or Convolutional layer. 
     Ref: He, Kaiming, et al. "Delving deep into rectifiers: Surpassing 
-    human-level performance on imagenet classificati
+    human-level performance on imagenet classification." Proceedings of the 
+    IEEE international conference on computer vision. 2015.
+    """
+    
+    if layer.weight.ndimension() == 4:
+        (n_o
