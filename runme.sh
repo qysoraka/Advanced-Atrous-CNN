7437444f3341f490f97fe3086acf83b643ca7ab0
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-12-24 12:47:26 +0000

    Add models

--- runme.sh
+++ runme.sh
@@ -6,4 +6,8 @@ DATASET_DIR="/home/renzhao/dcase2018"
 WORKSPACE="/home/renzhao/dcase2018/pub_dcase2018_crnn"
 
 DEV_SUBTASK_A_DIR="development-subtaskA"
-DEV_SUBTASK_B_DIR="development-subtaskB-
+DEV_SUBTASK_B_DIR="development-subtaskB-mobile"
+LB_SUBTASK_A_DIR="leaderboard-subtaskA"
+LB_SUBTASK_B_DIR="leaderboard-subtaskB-mobile"
+EVAL_SUBTASK_A_DIR="evaluation-subtaskA"
+EVAL_SUBT
