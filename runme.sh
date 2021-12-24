c43e5824a839d056ec24942c4720f0d07c76df1c
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-12-24 08:40:12 +0000

    ಠ_ಠ

--- runme.sh
+++ runme.sh
@@ -2,4 +2,8 @@
 # You need to modify this path
 DATASET_DIR="/home/renzhao/dcase2018"
 
-# You need to modify th
+# You need to modify this path as your workspace
+WORKSPACE="/home/renzhao/dcase2018/pub_dcase2018_crnn"
+
+DEV_SUBTASK_A_DIR="development-subtaskA"
+DEV_SUBTASK_B_DIR="development-subtaskB-
