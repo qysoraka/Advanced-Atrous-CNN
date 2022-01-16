c3cc900f9e64f840fbdbd1e0aa3bbf5bfdf33f16
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-01-16 11:17:58 +0000

    Replace ci.o.o links with docs.o.o/infra

--- runme.sh
+++ runme.sh
@@ -32,4 +32,5 @@ python $BACKEND/main_pytorch.py train --dataset_dir=$DATASET_DIR --subdir=$DEV_S
 #python $BACKEND/main_pytorch.py inference_validation_data --dataset_dir=$DATASET_DIR --subdir=$DEV_SUBTASK_A_DIR --workspace=$WORKSPACE --holdout_fold=$HOLDOUT_FOLD --iteration=15000 --cuda
 
 ############ Development subtask B ############
-# Train
+# Train model for subtask B
+#python $BACKEND/main_pytorch.py train --dataset_dir=$DATASET_DIR --subdir=$DEV_SUBTASK_B_DIR --workspace=$WORKSPACE --validate --hold
