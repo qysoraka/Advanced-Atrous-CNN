4309b84d4a1040a43a333f12538bab80d32c6327
qysoraka <156439778+qysoraka@users.noreply.github.com>
2023-04-19 14:15:51 +0000

    Fixes test.

--- utils/plot_figures.py
+++ utils/plot_figures.py
@@ -7,4 +7,11 @@ import config
 
 
 def plot_logmel(args):
-    """Plot log Mel feature of one audio per cl
+    """Plot log Mel feature of one audio per class. 
+    """
+
+    # Arguments & parameters
+    audios_dir = args.audios_dir
+    
+    sample_rate = config.sample_rate
+    window_size = config.window
